148915279ace828d0909bc20efc6e52262036ae3
Indira Williams <indirajamurath@gmail.com>
2018-07-11 13:15:30 -0400

    add user welcome page and links to it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,17 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def welcome
+		@user = current_user
+	end
+
 	def create
 
 		@user = User.new(user_params)
 
 		if @user.save
 			session[:user_id] = @user.id
-			redirect_to user_path(@user)
+			redirect_to welcome_path(@user)
 		else
 			render :new
 		end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:new, :create, :show, :edit, :update]
   resources :alcohols
   resources :user_alcohols
   resources :stores
   resources :store_alcohols
 
-  
-
+  get '/welcome', to: 'users#welcome', as: 'welcome'
   get '/login', to: 'sessions#new', as: 'login'
   get '/home', to: 'users#home', as: 'user_home'
   post '/sessions', to: "sessions#create", as: "sessions"
